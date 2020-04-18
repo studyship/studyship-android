ccd6ccbe600d31325538d48eefdc885fdcbfaafc
TaeseongYun <yts0646@gmail.com>
2020-04-19 00:00:04 +0900

    fix: modify apply study design

--- app/src/main/java/com/studyship/application/ui/activity/StudyApplyActivity.kt
+++ app/src/main/java/com/studyship/application/ui/activity/StudyApplyActivity.kt
@@ -1,8 +1,17 @@
 package com.studyship.application.ui.activity
 
+import android.graphics.Color
+import android.graphics.Typeface
+import android.graphics.Typeface.BOLD
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Spannable
+import android.text.SpannableStringBuilder
+import android.text.Spanned
+import android.text.style.ForegroundColorSpan
+import android.text.style.StyleSpan
 import com.studyship.application.R
+import kotlinx.android.synthetic.main.activity_study_apply_body_layout.*
 import kotlinx.android.synthetic.main.apply_study_header_bar.*
 
 class StudyApplyActivity : AppCompatActivity() {
@@ -12,5 +21,13 @@ class StudyApplyActivity : AppCompatActivity() {
         setContentView(R.layout.activity_study_apply)
 
         back_img.setOnClickListener { finish() }
+
+        val spannable = SpannableStringBuilder(apply_study_captain_name.text)
+        spannable.setSpan(
+            StyleSpan(BOLD),
+            3, // start
+            6, // end
+            Spannable.SPAN_EXCLUSIVE_INCLUSIVE
+        )
     }
 }
