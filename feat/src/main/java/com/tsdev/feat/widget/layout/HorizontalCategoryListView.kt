1ad760ff1c125ee2309c8447600f19543bd21cc7
TaeseongYun <yts0646@gmail.com>
2020-07-03 23:56:45 +0900

    PR #5 Feat : HorizontalCategoryListView'

---
+++ feat/src/main/java/com/tsdev/feat/widget/layout/HorizontalCategoryListView.kt
@@ -0,0 +1,13 @@
+package com.tsdev.feat.widget.layout
+
+import android.content.Context
+import android.util.AttributeSet
+import android.widget.HorizontalScrollView
+
+class HorizontalCategoryListView @JvmOverloads constructor(
+    context: Context,
+    attrs: AttributeSet,
+    defAttrs: Int = 0
+) : HorizontalScrollView(context, attrs, defAttrs) {
+
+}
