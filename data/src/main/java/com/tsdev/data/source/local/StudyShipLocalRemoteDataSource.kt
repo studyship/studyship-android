995ebab7961a12808f50e7ea96ced22082e74815
TaeseongYun <yts0646@gmail.com>
2020-07-16 14:11:20 +0900

    ISSUE #6 Feat : remote local interface

---
+++ data/src/main/java/com/tsdev/data/source/local/StudyShipLocalRemoteDataSource.kt
@@ -0,0 +1,7 @@
+package com.tsdev.data.source.local
+
+import com.tsdev.data.Local
+
+interface StudyShipLocalRemoteDataSource : Local {
+
+}
---
+++ data/src/main/java/com/tsdev/data/source/remote/StudyShipRemoteDataSource.kt
@@ -0,0 +1,7 @@
+package com.tsdev.data.source.remote
+
+import com.tsdev.data.Remote
+
+interface StudyShipRemoteDataSource : Remote{
+
+}
