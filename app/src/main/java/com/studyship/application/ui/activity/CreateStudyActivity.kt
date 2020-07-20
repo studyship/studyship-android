d68881dfadabcd0beaa0666a94b818365ab34b1d
TaeseongYun <yts0646@gmail.com>
2020-07-20 16:27:52 +0900

    ISSUE #18 Feat : 유저가 버튼 클릭에 따라 다른 액티비티(스터디생성, 임시저장 액티비티) 분기처리 완료

---
+++ app/src/main/java/com/studyship/application/ui/activity/CreateStudyActivity.kt
@@ -0,0 +1,13 @@
+package com.studyship.application.ui.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.studyship.application.R
+
+class CreateStudyActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_create_study)
+    }
+}
---
+++ app/src/main/java/com/studyship/application/ui/activity/TemporaryStudyActivity.kt
@@ -0,0 +1,13 @@
+package com.studyship.application.ui.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.studyship.application.R
+
+class TemporaryStudyActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_temporary_study)
+    }
+}
