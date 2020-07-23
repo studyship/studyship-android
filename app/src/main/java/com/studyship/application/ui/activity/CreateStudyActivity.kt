491dc2d79f767176c4b303c48ae23334233d4de2
TaeseongYun <yts0646@gmail.com>
2020-07-24 01:24:19 +0900

    Modify : HorizontalDateLayout

--- app/src/main/java/com/studyship/application/ui/activity/CreateStudyActivity.kt
+++ app/src/main/java/com/studyship/application/ui/activity/CreateStudyActivity.kt
@@ -3,11 +3,14 @@ package com.studyship.application.ui.activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.studyship.application.R
+import kotlinx.android.synthetic.main.activity_create_study.*
 
 class CreateStudyActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_create_study)
+
+        gather_date_layout.setLayoutData(listOf("일", "월", "화", "수", "목", "금", "토"))
     }
 }
--- feat/src/main/java/com/tsdev/feat/widget/layout/HorizontalGatherDateLayout.kt
+++ feat/src/main/java/com/tsdev/feat/widget/layout/HorizontalGatherDateLayout.kt
@@ -2,15 +2,58 @@ package com.tsdev.feat.widget.layout
 
 import android.content.Context
 import android.util.AttributeSet
+import android.view.Gravity
+import android.view.LayoutInflater
 import android.view.View
 import android.widget.HorizontalScrollView
+import android.widget.LinearLayout
+import com.tsdev.feat.R
+import kotlinx.android.synthetic.main.layout_user_select_date.view.*
 
 class HorizontalGatherDateLayout @JvmOverloads constructor(
     context: Context,
     attrs: AttributeSet,
     defAttrs: Int = 0
 ) : HorizontalScrollView(context, attrs, defAttrs) {
-    val userSelectedDate = mutableListOf<Pair<View, String>>()
 
+    val userSelectedDate = mutableListOf<String>()
+
+    private val defaultParentView: LinearLayout by lazy {
+        LinearLayout(context).apply {
+            orientation = LinearLayout.HORIZONTAL
+            gravity = Gravity.START
+        }
+    }
+
+    fun setLayoutData(items: List<String>) {
+        val dateLayouts = items.mapIndexed { position, value ->
+
+            fun layoutItem(value: String): View {
+                return LayoutInflater.from(context).inflate(
+                    R.layout.layout_user_select_date,
+                    this@HorizontalGatherDateLayout,
+                    false
+                ).apply {
+                    date_string.text = value
+                    setOnClickListener { view ->
+                        userSelectedDate.getOrNull(position)?.run {
+                            userSelectedDate.removeAt(userSelectedDate.indexOf(value))
+                            view.isSelected = false
+                        } ?: run {
+                            userSelectedDate.add(value)
+                            view.isSelected = true
+                        }
+                    }
+                }
+            }
+
+            val button = layoutItem(value)
+
+            button
+        }
+        dateLayouts.forEach(defaultParentView::addView)
+
+        addView(defaultParentView)
+    }
 
 }
