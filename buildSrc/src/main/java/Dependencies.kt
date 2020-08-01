b8e5abba8edc06133870cce22ded95296492b94f
TaeseongYun <yts0646@gmail.com>
2020-08-01 23:31:41 +0900

    feat: Kotlin DSL

---
+++ buildSrc/src/main/java/Dependencies.kt
@@ -0,0 +1,11 @@
+object Apps {
+    const val COMPILE_SDK = 29
+    const val MIN_SDK = 23
+    const val TARGET_SDK = 29
+    const val VERSION_CODE = 1
+    const val VERSION_NAME = "1.0"
+}
+
+object Versions {
+    const val ANDROID_PLUGIN_VERSION = "4.0.1"
+}
