e31182107c7d55859faa6eada5635682979679f0
TaeseongYun <yts0646@gmail.com>
2020-08-18 23:53:48 +0900

    modify : MystudyAppliedFragment

--- app/src/main/java/com/studyship/application/base/fragment/BaseFragment.kt
+++ app/src/main/java/com/studyship/application/base/fragment/BaseFragment.kt
@@ -8,14 +8,15 @@ import androidx.annotation.LayoutRes
 import androidx.databinding.DataBindingUtil
 import androidx.databinding.ViewDataBinding
 import androidx.fragment.app.Fragment
+import com.tsdev.presentation.base.BaseViewModel
 
-abstract class BaseFragment<VIEW_DATA_BINDING : ViewDataBinding>(@LayoutRes private val layout: Int) :
+abstract class BaseFragment<VIEW_DATA_BINDING : ViewDataBinding, VIEW_MODEL : BaseViewModel>(@LayoutRes private val layout: Int) :
     Fragment() {
 
     protected lateinit var viewDataBinding: VIEW_DATA_BINDING
         private set
 
-
+    abstract val viewModel: VIEW_MODEL
 
     override fun onCreateView(
         inflater: LayoutInflater,
--- app/src/main/java/com/studyship/application/ui/fragment/viewpager/MyStudyAppliedFragment.kt
+++ app/src/main/java/com/studyship/application/ui/fragment/viewpager/MyStudyAppliedFragment.kt
@@ -6,18 +6,35 @@ import android.view.View
 import com.studyship.application.R
 import com.studyship.application.base.fragment.BaseFragment
 import com.studyship.application.databinding.FragmentMyStudyAppliedBinding
+import com.tsdev.presentation.MyStudyViewModel
+import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class MyStudyAppliedFragment :
-    BaseFragment<FragmentMyStudyAppliedBinding>(R.layout.fragment_my_study_applied) {
+    BaseFragment<FragmentMyStudyAppliedBinding, MyStudyViewModel>(R.layout.fragment_my_study_applied) {
+
+    override val viewModel: MyStudyViewModel by viewModel()
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        bind {
 
+        bind {
+            vm = viewModel
+            executePendingBindings()
         }
 
         viewDataBinding.horizontalFilter.setCategoryOnClickListener = {
             //todo recycler Refresh
+            when (it) {
+                PENDING_ACCEPTANCE -> {
+
+                }
+                RECRUITING_MEMBERS -> {
+
+                }
+                WAITING -> {
+
+                }
+            }
             Log.e("CLICK", it.toString())
         }
 
@@ -25,5 +42,11 @@ class MyStudyAppliedFragment :
             setLayoutData(listOf("수락대기중", "멤버모집중", "대기중"), R.layout.recycler_my_study_item)
             initSelected()
         }
+    }
+
+    companion object {
+        private const val PENDING_ACCEPTANCE = 0
+        private const val RECRUITING_MEMBERS = 1
+        private const val WAITING = 2
     }
 }
