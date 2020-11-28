fbea3a27561b53c36ad5a4827a2fce6fbf233bce
Taeseong Yun <yts0646@gmail.com>
2020-11-28 15:38:20 +0900

    PR: 프로필  UI (#40)
    * feat: 프로필 INIT UI:
    
    * modify : MyProfile UI

--- app/src/main/java/com/studyship/application/ui/fragment/MyProfileFragment.kt
+++ app/src/main/java/com/studyship/application/ui/fragment/MyProfileFragment.kt
@@ -1,16 +1,13 @@
 package com.studyship.application.ui.fragment
 
-import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import androidx.fragment.app.Fragment
 import com.studyship.application.R
+import com.studyship.application.base.fragment.BaseFragment
+import com.studyship.application.databinding.FragmentMyprofileLayoutBinding
+import com.tsdev.presentation.MyProfileViewModel
+import org.koin.androidx.viewmodel.ext.android.viewModel
 
-class MyProfileFragment : Fragment() {
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? = inflater.inflate(R.layout.fragment_myprofile_layout, container, false)
+class MyProfileFragment :
+    BaseFragment<FragmentMyprofileLayoutBinding, MyProfileViewModel>(R.layout.fragment_myprofile_layout) {
+
+    override val viewModel: MyProfileViewModel by viewModel()
 }
